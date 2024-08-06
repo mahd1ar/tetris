a91c705dddb5a9403e71de6d6f15fbea793aa018
mahdiyar <a.mahdiyar7@yahoo.com>
2024-08-06 15:08:57 +0330

    feat: Improved game logic and user interaction with event handling, shape rotation, and block movement.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    io::stdout,
+    borrow::Borrow,
+    io::{stdout, Write},
+    process,
     sync::{Arc, Mutex},
     thread,
     time::Duration,
@@ -8,45 +10,190 @@ use std::{
 use anyhow::Result;
 use crossterm::{
     cursor::MoveTo,
-    style::Print,
+    event::{
+        read, DisableBracketedPaste, DisableFocusChange, DisableMouseCapture, EnableBracketedPaste,
+        EnableFocusChange, EnableMouseCapture, Event, KeyCode,
+    },
+    execute,
+    style::{self, style, Print, Stylize},
     terminal::{self, ClearType},
-    ExecutableCommand,
+    ExecutableCommand, QueueableCommand,
 };
+
+#[derive(Clone)]
+enum Color {
+    CYAN,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+}
+
+#[derive(PartialEq, Clone)]
+enum Shape {
+    I,
+    L,
+    O,
+    S,
+    T,
+}
+
+struct Block {
+    color: Color,
+    shape: Shape,
+    rotation: usize,
+    x: usize,
+    y: usize,
+}
+
+struct SolidBlock {
+    color: Color,
+}
+
+#[derive(Clone)]
+struct Cell {
+    filled: bool,
+    color: Color,
+}
+
+fn print_events(act: Arc<Mutex<KeyAction>>) -> Result<()> {
+    execute!(
+        std::io::stdout(),
+        EnableBracketedPaste,
+        EnableFocusChange,
+        EnableMouseCapture
+    )?;
+    loop {
+        // `read()` blocks until an `Event` is available
+        match read()? {
+            Event::FocusGained => println!("FocusGained"),
+            Event::FocusLost => println!("FocusLost"),
+            Event::Key(event) => {
+                *act.lock().unwrap() = match event.code {
+                    KeyCode::Char('w') => KeyAction::U,
+                    KeyCode::Char('s') => KeyAction::D,
+                    KeyCode::Char('a') => KeyAction::L,
+                    KeyCode::Char('d') => KeyAction::R,
+                    _ => KeyAction::N,
+                };
+
+                if event.code == KeyCode::Char('c')
+                    && event.modifiers == crossterm::event::KeyModifiers::CONTROL
+                {
+                    println!("\n Exiting... \n goodbye!");
+                    process::exit(0);
+                }
+            }
+            Event::Resize(width, height) => println!("New size {}x{}", width, height),
+            // case control + c to exit
+            _ => (),
+        }
+    }
+    execute!(
+        std::io::stdout(),
+        DisableBracketedPaste,
+        DisableFocusChange,
+        DisableMouseCapture
+    )?;
+    Ok(())
+}
+
+#[derive(Debug)]
+enum KeyAction {
+    U,
+    D,
+    L,
+    R,
+    N,
+}
 
 fn main() -> Result<()> {
     println!("begin!");
+
     let mut stdout = stdout();
-
-    let state: Arc<Mutex<Vec<Vec<usize>>>> = Arc::new(Mutex::new(vec![vec!(3; 3); 0]));
-
+    let (win_width, win_height) = terminal::size().unwrap();
+    let state: Arc<Mutex<Vec<Vec<Cell>>>> = Arc::new(Mutex::new(vec![
+        vec![
+            Cell {
+                color: Color::CYAN,
+                filled: false
+            };
+            win_height as usize
+        ];
+        win_width as usize
+    ]));
     let nstate = Arc::clone(&state);
-    thread::spawn(move || {
-        // block
 
-        thread::sleep(Duration::new(3, 0));
-        nstate.lock().unwrap().push(vec![0]);
+    let action = Arc::new(Mutex::new(KeyAction::N));
+    let naction = Arc::clone(&action);
+
+    let candidateBlock = Arc::new(Mutex::new(Block {
+        color: Color::RED,
+        shape: Shape::I,
+        rotation: 0,
+        x: 5,
+        y: 0,
+    }));
+
+    let candidateBlock_1 = Arc::clone(&candidateBlock);
+
+    thread::spawn(move || {
+        print_events(naction);
+    });
+
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(1));
+        let lock = candidateBlock_1.lock();
+        lock.unwrap().y += 1;
     });
 
     loop {
         stdout.execute(terminal::Clear(ClearType::All))?;
-        thread::sleep(Duration::from_millis(500));
 
-        for x in 0..state.lock().unwrap().len() {
-            for y in 0..state.lock().unwrap()[0].len() {
-                let cord = state.lock().unwrap()[x][y];
+        let mut  candidateBlock = candidateBlock.lock().unwrap();
+        
+        match *action.lock().unwrap() {
+            KeyAction::L => {
+                candidateBlock.x -= 1;
+            }
+            KeyAction::R => {
+                candidateBlock.x += 1;
+            }
+            KeyAction::D => {
+                candidateBlock.y += 1;
+            }
+            _ => {}
+        }
+        if candidateBlock.shape == Shape::I {
+            state.lock().unwrap()[candidateBlock.x][candidateBlock.y].filled = true;
+            state.lock().unwrap()[candidateBlock.x][candidateBlock.y + 1].filled = true;
+            state.lock().unwrap()[candidateBlock.x][candidateBlock.y + 2].filled = true;
+            state.lock().unwrap()[candidateBlock.x][candidateBlock.y + 3].filled = true;
+            state.lock().unwrap()[candidateBlock.x][candidateBlock.y + 3].color =
+                candidateBlock.color.clone();
+        }
 
-                let blk = match cord {
-                    1 => "]",
-                    0 => "x",
-                    _ => "_",
+        // draw
+
+        for (x, i) in state.lock().unwrap().iter().enumerate() {
+            for (y, j) in i.iter().enumerate() {
+                let blk = match j.filled {
+                    true => "██",
+                    false => " ",
                 };
 
+                // println!("{}", blk);
+
                 stdout
-                    .execute(MoveTo(x as u16, y as u16))?
-                    .execute(Print(blk))?;
+                    .queue(MoveTo(x as u16, y as u16))?
+                    .queue(style::PrintStyledContent(blk.cyan()))?;
+
+                stdout.flush();
             }
         }
-        // println!("{:?}" , state);
+
+        *action.lock().unwrap() = KeyAction::N;
+
         thread::sleep(Duration::from_millis(500));
     }
 
